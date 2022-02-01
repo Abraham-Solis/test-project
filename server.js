7b65dc247ec718eefedeb39d9b310ac6a6c982b0
email <abe.solis2015@gmail.com>
2022-02-01 12:12:51 -0800

    code

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ passport.use(new JWTStrategy({
 
 app.use(require('./routes'))
 
-app.get('*', (req, res) => res.sendFile(join(__dirname, 'client', 'public', 'index.html')))
+app.get('*', (req, res) => res.sendFile(join(__dirname, 'client', 'build', 'index.html')))
 
 require('./db')
   .then(() => app.listen(process.env.PORT || 3001))
